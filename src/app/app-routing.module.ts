be42cfe6772763572fa6a7d2ae6808b9f593ea95
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-03 17:14:35 +0300

    idk anymore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { GameuserListComponent } from './gameuser-list/gameuser-list.component';
 import { CreateGameuserComponent } from './create-gameuser/create-gameuser.component';
 import { GameuserAverageplayercharacterlevelComponent } from './gameuser-averageplayercharacterlevel/gameuser-averageplayercharacterlevel.component';
 import { UpdateGameuserComponent } from './update-gameuser/update-gameuser.component';
+import { GameuserDetailsComponent } from './gameuser-details/gameuser-details.component';
 
 const routes: Routes = [
   {path:'gameusers',component:GameuserListComponent}, //in app.component.html <a routerLink="gameusers" class="nav-link"> GameUser List</a>
   //{path:'',redirectTo:'gameusers',pathMatch:'full'},//empty /localhost:4200 gets us directly to this
   {path:'create-gameuser',component:CreateGameuserComponent},
   {path:'gameuser-average-playercharacter-level',component:GameuserAverageplayercharacterlevelComponent},
-  {path:'update-gameuser/:id', component:UpdateGameuserComponent}
+  {path:'update-gameuser/:id', component:UpdateGameuserComponent},
+  {path:'gameuser-details/:id',component:GameuserDetailsComponent}
 ];
 //maybe remove /:id from update-gameuser route
 @NgModule({
