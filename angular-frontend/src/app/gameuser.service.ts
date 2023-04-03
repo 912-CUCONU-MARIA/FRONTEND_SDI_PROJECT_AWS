fae22397165432141eced27fc14aab69fa9ccd57
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-03 18:37:05 +0300

    removed api from mapping

--- angular-frontend/src/app/gameuser.service.ts
+++ angular-frontend/src/app/gameuser.service.ts
@@ -10,32 +10,31 @@ export class GameuserService {
 
   //change this for cloud ip
   //private baseURL="http://localhost:80/gameusers"
-  //private baseURL="http://13.48.3.156:80/gameusers"
-  private baseURL="http://13.48.3.156:80/api"
+  private baseURL="http://13.48.3.156:80/gameusers"
   constructor(private httpClient:HttpClient) { }
 
   getGameusersList():Observable<Gameuser[]>{
-      return this.httpClient.get<Gameuser[]>(`${this.baseURL}/gameusers`);
+      return this.httpClient.get<Gameuser[]>(`${this.baseURL}`);
   }
 
   createGameuser(gameuser:Gameuser):Observable<Object>{
-    return this.httpClient.post(`${this.baseURL}/gameusers`,gameuser);
+    return this.httpClient.post(`${this.baseURL}`,gameuser);
   }
 
   getGameUsersOrderedByAverageLevelOfPlayerCharacters(): Observable<Gameuseraverageplayercharacterleveldto[]> {
-    return this.httpClient.get<Gameuseraverageplayercharacterleveldto[]>(`${this.baseURL}/gameusers/averageplayercharacterlevel`);
+    return this.httpClient.get<Gameuseraverageplayercharacterleveldto[]>(`${this.baseURL}/averageplayercharacterlevel`);
   }
 
   getGameuserById(id:number): Observable<Gameuser>{
-    return this.httpClient.get<Gameuser>(`${this.baseURL}/gameusers/${id}`);
+    return this.httpClient.get<Gameuser>(`${this.baseURL}/${id}`);
   }
 
   updateGameuser(id:number, gameuser:Gameuser):Observable<Object>{
-    return this.httpClient.put(`${this.baseURL}/gameusers/${id}`,gameuser);
+    return this.httpClient.put(`${this.baseURL}/${id}`,gameuser);
   }
 
   deleteGameuser(id:number):Observable<Object>{
-    return this.httpClient.delete(`${this.baseURL}/gameusers/${id}`);
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
 
 }
