7ff0e1269d4a1f925b559f85a4a70c43d90934a8
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-23 13:30:54 +0300

    Update gameuser-averageplayercharacterlevel.component.ts

--- angular-frontend/src/app/gameuser-averageplayercharacterlevel/gameuser-averageplayercharacterlevel.component.ts
+++ angular-frontend/src/app/gameuser-averageplayercharacterlevel/gameuser-averageplayercharacterlevel.component.ts
@@ -1,47 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Gameuseraverageplayercharacterleveldto } from '../gameuseraverageplayercharacterleveldto'; 
-import { GameuserService } from '../gameuser.service';
+  import { Component, OnInit } from '@angular/core';
+  import { Gameuseraverageplayercharacterleveldto } from '../gameuseraverageplayercharacterleveldto'; 
+  import { GameuserService } from '../gameuser.service';
 
-@Component({
-  selector: 'app-gameuser-averageplayercharacterlevel',
-  templateUrl: './gameuser-averageplayercharacterlevel.component.html',
-  styleUrls: ['./gameuser-averageplayercharacterlevel.component.css']
-})
-export class GameuserAverageplayercharacterlevelComponent implements OnInit{
-  gameuseraverageplayercharacterleveldtos?: Gameuseraverageplayercharacterleveldto[]; 
+  @Component({
+    selector: 'app-gameuser-averageplayercharacterlevel',
+    templateUrl: './gameuser-averageplayercharacterlevel.component.html',
+    styleUrls: ['./gameuser-averageplayercharacterlevel.component.css']
+  })
+  export class GameuserAverageplayercharacterlevelComponent implements OnInit{
+    gameuseraverageplayercharacterleveldtos?: Gameuseraverageplayercharacterleveldto[]; 
 
 
-  //for pagination
-  totalPages: number[] = [];
-  totalElements: number = 0;
-  currentPage: number = 0;
-  pageSize: number = 10;
+    //for pagination
+    totalPages: number[] = [];
+    totalElements: number = 0;
+    currentPage: number = 0;
+    pageSize: number = 10;
 
-  constructor(private gameuserService:GameuserService){}
+    constructor(private gameuserService:GameuserService){}
 
-  ngOnInit(): void {
-    this.getGameuseraverageplayercharacterleveldtos();
+    ngOnInit(): void {
+      this.getGameuseraverageplayercharacterleveldtos();
+    }
+
+    private getGameuseraverageplayercharacterleveldtos(){
+      this.gameuserService.getGameUsersOrderedByAverageLevelOfPlayerCharacters(this.currentPage,this.pageSize).subscribe(data=>
+        {
+          this.gameuseraverageplayercharacterleveldtos=data.content;
+          this.totalElements=data.totalElements;
+          this.totalPages = Array.from({ length: data.totalPages }, (_, i) => i);
+        });
+    }
+
+    setPage(page: number) {
+      this.currentPage = page;
+      this.getGameuseraverageplayercharacterleveldtos();
+    }
+
+    get pageRange() {
+      const left = Math.max(0, this.currentPage - 2);
+      const right = Math.min(this.totalPages.length - 1, this.currentPage + 2);
+      const withDots = [...Array(this.totalPages.length).keys()].filter(i => i === 0 || i === this.totalPages.length - 1 || (i >= left && i <= right));
+      return withDots;
+    }
+
   }
-
-  private getGameuseraverageplayercharacterleveldtos(){
-    this.gameuserService.getGameUsersOrderedByAverageLevelOfPlayerCharacters(this.currentPage,this.pageSize).subscribe(data=>
-      {
-        this.gameuseraverageplayercharacterleveldtos=data.content;
-        this.totalElements=data.totalElements;
-        this.totalPages = Array.from({ length: data.totalPages }, (_, i) => i);
-      });
-  }
-
-  setPage(page: number) {
-    this.currentPage = page;
-    this.getGameuseraverageplayercharacterleveldtos();
-  }
-
-  get pageRange() {
-    const left = Math.max(0, this.currentPage - 2);
-    const right = Math.min(this.totalPages.length - 1, this.currentPage + 2);
-    const withDots = [...Array(this.totalPages.length).keys()].filter(i => i === 0 || i === this.totalPages.length - 1 || (i >= left && i <= right));
-    return withDots;
-  }
-
-}
