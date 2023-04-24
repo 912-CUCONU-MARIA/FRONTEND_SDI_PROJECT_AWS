96489e859030f51596588282b8838778f4697c06
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-04-24 18:40:38 +0300

    Update app.module.ts

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameuserListComponent } from './gameuser-list/gameuser-list.component';
+import { GameuserListComponent } from './gameuser_folder/gameuser-list/gameuser-list.component';
 import { CreateGameuserComponent } from './gameuser_folder/create-gameuser/create-gameuser.component';
 import { FormsModule } from '@angular/forms';
 import { GameuserAverageplayercharacterlevelComponent } from './gameuser_folder/gameuser-averageplayercharacterlevel/gameuser-averageplayercharacterlevel.component';
-import { UpdateGameuserComponent } from './update-gameuser/update-gameuser.component';
-import { GameuserDetailsComponent } from './gameuser-details/gameuser-details.component';
+import { UpdateGameuserComponent } from './gameuser_folder/update-gameuser/update-gameuser.component';
+import { GameuserDetailsComponent } from './gameuser_folder/gameuser-details/gameuser-details.component';
 import { ItemListComponent } from './item_folder/item-list/item-list.component';
 import { CreateItemComponent } from './item_folder/create-item/create-item.component';
 
