d8000e563f0ae05c22894eab938fc72a3eaf26e8
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-05-04 14:45:21 +0300

    added pcharacter frontend to app module

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { ItemListComponent } from './item_folder/item-list/item-list.component';
 import { CreateItemComponent } from './item_folder/create-item/create-item.component';
 import { UpdateItemComponent } from './item_folder/update-item/update-item.component';
 import { ItemDetailsComponent } from './item_folder/item-details/item-details.component';
+import { PlayercharacterListComponent } from './playercharacter_folder/playercharacter-list/playercharacter-list.component';
+import { CreatePlayercharacterComponent } from './playercharacter_folder/create-playercharacter/create-playercharacter.component';
+import { UpdatePlayercharacterComponent } from './playercharacter_folder/update-playercharacter/update-playercharacter.component';
+import { PlayercharacterDetailsComponent } from './playercharacter_folder/playercharacter-details/playercharacter-details.component';
 
 const routes: Routes = [
   {path:'gameusers',component:GameuserListComponent}, //in app.component.html <a routerLink="gameusers" class="nav-link"> GameUser List</a>
@@ -21,7 +25,12 @@ const routes: Routes = [
   {path:'items',component:ItemListComponent},
   {path:'create-item',component:CreateItemComponent},
   {path:'update-item/:id', component:UpdateItemComponent},
-  {path:'item-details/:id',component:ItemDetailsComponent}
+  {path:'item-details/:id',component:ItemDetailsComponent},
+
+  {path:'playercharacters',component:PlayercharacterListComponent},
+  {path:'create-playercharacter',component:CreatePlayercharacterComponent},
+  {path:'update-playercharacter/:id', component:UpdatePlayercharacterComponent},
+  {path:'playercharacter-details/:id',component:PlayercharacterDetailsComponent}
 
 ];
 //maybe remove /:id from update-gameuser route
