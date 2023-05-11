79c9b26e8559cd6b9f1066cba8b87c7a8e923a0c
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-05-11 21:03:26 +0300

    Update gameuser-list.component.ts

--- angular-frontend/src/app/gameuser_folder/gameuser-list/gameuser-list.component.ts
+++ angular-frontend/src/app/gameuser_folder/gameuser-list/gameuser-list.component.ts
@@ -49,38 +49,39 @@ export class GameuserListComponent implements OnInit{
 
   get pageRange() {
     let result: number[] = [];
-  
+
     // Always add the first 5 pages and the last 5 pages
-    for (let i = 0; i < 5; i++) {
-      if (!result.includes(i)) result.push(i);
-      if (!result.includes(this.totalPages.length - i - 1)) result.push(this.totalPages.length - i - 1);
+    for (let i = 1; i <= 5; i++) {
+        if (!result.includes(i)) result.push(i);
+        if (!result.includes(this.totalPages.length - i + 1)) result.push(this.totalPages.length - i + 1);
     }
-  
+
     // Handling the first 10 pages
     if (this.currentPage < 10) {
-      for (let i = 1; i <= this.currentPage + 6; i++) {
-        if (!result.includes(i)) result.push(i);
-      }
+        for (let i = 1; i <= this.currentPage + 6; i++) {
+            if (!result.includes(i)) result.push(i);
+        }
     }
-  
+
     // Handling middle pages
-    else if (this.currentPage >= 10 && this.currentPage <= this.totalPages.length - 12) {
-      for (let i = this.currentPage - 4; i <= this.currentPage + 6; i++) {
-        if (!result.includes(i)) result.push(i);
-      }
+    else if (this.currentPage >= 10 && this.currentPage < this.totalPages.length - 11) {
+        for (let i = this.currentPage - 4; i <= this.currentPage + 6; i++) {
+            if (!result.includes(i)) result.push(i);
+        }
     }
-  
+
     // Handling last 12 pages
-    else if (this.currentPage > this.totalPages.length - 12) {
-      for (let i = this.currentPage - 4; i <= this.currentPage; i++) {
-        if (!result.includes(i)) result.push(i);
-      }
+    else if (this.currentPage >= this.totalPages.length - 11) {
+        for (let i = this.currentPage - 4; i <= this.currentPage + 1; i++) {
+            if (!result.includes(i)) result.push(i);
+        }
     }
-  
+
     // Sort the array and return it
     result.sort((a, b) => a - b);
     return result;
-  }
+}
+
   
   
   
