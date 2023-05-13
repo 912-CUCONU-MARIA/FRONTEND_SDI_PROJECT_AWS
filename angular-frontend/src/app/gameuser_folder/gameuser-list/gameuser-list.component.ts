4cb737ab6e99d27581da486be2f6c8374e278f73
MariaCuconu <maria.cuconu@stud.ubbcluj.ro>
2023-05-13 19:03:55 +0300

    testing development branch - removed some commits

--- angular-frontend/src/app/gameuser_folder/gameuser-list/gameuser-list.component.ts
+++ angular-frontend/src/app/gameuser_folder/gameuser-list/gameuser-list.component.ts
@@ -98,21 +98,6 @@ export class GameuserListComponent implements OnInit{
       return gapWithNextPage > 1 && gapWithPreviousPage === 1;
     }
     
-    
-    //1 2 3 4 5 6 ... 44 45 46 47 48
-    //1 2 3 4 5 6 7 ... 44 45 46 47 48
-    //1 2 3 4 5 ... 6 7 8 9 10 11 12 13 14 15 16 ... 44 45 46 47 48
-    //1 2 3 4 5 ... 31 32 33 34 35 36 37 38 39 40 41 ... 44 45 46 47 48
-    //1 2 3 4 5 ... 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
-    
-    
-    
-    //
-
-
-   
-  
-
 
   updateGameuser(id: number){
     this.router.navigate(['update-gameuser',id]);
